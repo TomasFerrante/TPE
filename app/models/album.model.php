515a4243d09586ca64e7b912bbe6cfc737b58ba5
Tomas Abel Ferrante <tomax19cr7@gmail.com>
2023-11-22 00:06:24 -0300

    cambios 22/11

---
+++ app/models/album.model.php
@@ -0,0 +1,45 @@
+<?php
+
+public class AlbumModel {
+
+    private $db;
+
+    public function __contruct()  {
+        $this->db = new PDO ('mysql:host=localhost;dbname=db_tienda_de_musica;charset=utf8', 'root', '');
+    }
+
+    public function getAlbum() {
+        $query = $this->db->prepare('SELECT * FROM album');
+        $query->execute();
+        $albumes = $query->fetchAll(PDO::FETCH_OBJ);
+        
+        return $albumes;
+    }
+
+    public function getAlbum($genero) {
+        $query = $this->db->prepare('SELECT * FROM album WHERE genero = ?');
+        $query->execute([$genero]);
+        $albumes = $query->fetchAll(PDO::FETCH_OBJ);
+        
+        return $albumes;
+    }
+
+    public function insertAlbum($nombre, $cant_canciones, $duracion, $artista, $genero, $lanzamiento, $precio) {
+        $query = $this->db->prepare('INSERT INTO albumes (nombre, canciones, duracion, artista, genero, lanzamiento, precio) VALUES (?,?,?,?,?,?,?)');
+        $query->execute([$nombre, $cant_canciones, $duracion, $artista, $genero, $lanzamiento, $precio]);
+        
+        return $this->db->lastInsertId();
+    }
+
+    public function deleteAlbum($id) {
+        $query = $this->db->prepare('DELETE FROM album WHERE id = ?');
+        $query->execute([$id]);
+    }
+
+    public function updateAlbum($id, $nombre, $cant_canciones, $duracion, $artista, $genero, $lanzamiento, $precio) {
+        $query = $this->db->prepare('UPDATE album SET nombre = ?, canciones = ?, duracion = ?, artista = ?, genero = ?, lanzamiento = ?, precio = ? WHERE id = ?');
+        $query->execute([$nombre, $cant_canciones, $duracion, $artista, $genero, $lanzamiento, $precio. $id]);
+    }
+}
+
+?>
